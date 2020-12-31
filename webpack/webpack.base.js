7faf894a24afe4c8df067633f29a09fc811d3844
Ayodeji <afolabiayodejia@gmail.com>
2020-12-31 16:56:09 +0100

    add favicon

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -42,6 +42,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebPackPlugin({
+      favicon: "./src/assets/favicon.ico",
       template: "./src/index.html",
       filename: "index.html",
       minify: {
